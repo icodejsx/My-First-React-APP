cf0f2e7aa78398fb2bac2fd24fb173b44e6ee938
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-01-18 17:11:56 +0100

    building

--- vite-project/src/components/counter.jsx
+++ vite-project/src/components/counter.jsx
@@ -4,12 +4,16 @@ const Counter = () => {
   const [value, setValue] = useState(1);
   return (
     <div>
-      <span>{value}</span>
+      <span>{formatCount}</span>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Increment
       </button>
     </div>
   );
+};
+const formatCount = () => {
+  const { count } = this.props;
+  return count === 0 ? "Zero" : count;
 };
 
 export default Counter;
