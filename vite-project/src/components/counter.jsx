f5bb5e9c2c5b10ea6b9ad4257b589dfb13ef058a
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-01-19 14:23:59 +0100

    learning

--- vite-project/src/components/counter.jsx
+++ vite-project/src/components/counter.jsx
@@ -1,19 +1,25 @@
 import React, { Component, useState } from "react";
 
 const Counter = () => {
-  const [value, setValue] = useState(1);
+  const [count] = useState(0);
+  const formatCount = (count) => (count === 0 ? "Zero" : count);
+  const badge = () => (count === 0 ? "bg-yellow-600" : "bg-blue-600");
+  const tags = ["tag1", "tag2", "tag3"];
+
   return (
     <div>
-      <span>{formatCount}</span>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <span className={`m-2 p-1 rounded text-base text-white ${badge()}`}>
+        {formatCount(count)}
+      </span>
+      <button className=" hover:bg-blue-600 text-black-200 font-medium py-2 px-4 rounded shadow-md">
         Increment
       </button>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     </div>
   );
 };
-const formatCount = () => {
-  const { count } = this.props;
-  return count === 0 ? "Zero" : count;
-};
-
 export default Counter;
